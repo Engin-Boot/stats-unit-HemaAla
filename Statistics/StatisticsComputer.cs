c6d0a84261c97266dd76e6a69c0861332bba330d
HemaAla <31954901+HemaAla@users.noreply.github.com>
2021-07-22 11:49:49 +0530

    made changes

--- Statistics/StatisticsComputer.cs
+++ Statistics/StatisticsComputer.cs
@@ -11,13 +11,13 @@ namespace Statistics
             double _min;
 
             if (numbers.Count == 0)
-                return new StattisticsData();
+                return new StatisticsData();
 
             _max = numbers[0];
             _min = numbers[0];
             _avg = 0.0m;
 
-            foreach(float number in numbers)
+            foreach(double number in numbers)
             {
                 if (number > _max)
                     _max = number;
