1ff7575c0cf511e8cb515a3a709053aec678ab5b
HemaAla <31954901+HemaAla@users.noreply.github.com>
2021-07-22 12:27:42 +0530

    made changes

--- Statistics/LedAlerter.cs
+++ Statistics/LedAlerter.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
-    public class LedAlert : IAlerter
+    public class LedAlerter : IAlerter
     {
         public bool _ledGlowed = false;
         public void Alert(double maxThreshold, double max)
